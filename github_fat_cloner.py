341143486fd2f44d481b1406736184d34b211843
Octave Larose <octave.larose@hotmail.fr>
2021-03-19 17:22:26 +0000

    Improving github-fat-cloner script a bit

--- github_fat_cloner.py
+++ github_fat_cloner.py
@@ -4,10 +4,12 @@ import shutil
 from github import Github
 import git
 
-MIN_LINES_NBR = 150000
-REPOS_PATH = "./repos"
-QUERY_STR = "language:java"
+QUERY_STR = "language:java fork:false"
+MIN_LINES_NBR = 200000
+MIN_NBR_CONTRIBUTORS = 3
+MIN_NBR_COMMITS = 20
 
+REPOS_PATH = "../../../../../java_codebases"
 
 class CloneProgressPrinter(git.remote.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
@@ -19,12 +21,7 @@ def get_repo_name_from_url(repo_url: str) -> str:
     return f"{split_repo_url[-2]}_{split_repo_url[-1][:-4]}"
 
 
-def clone_repo(repo_url: str, repos_path: str):
-    output_dir = os.path.join(repos_path, get_repo_name_from_url(repo_url))
-    if os.path.isdir(output_dir):
-        print(f"{output_dir} already exists, skipping cloning.")
-        return
-
+def clone_repo(repo_url: str, output_dir: str):
     print(f"Cloning {repo_url.split('/')[-1][:-4]}...")
     git.Repo.clone_from(repo_url, output_dir, progress=CloneProgressPrinter(), multi_options=["--depth=1"])
     shutil.rmtree(os.path.join(output_dir, ".git"))
@@ -38,18 +35,28 @@ def main():
         os.mkdir(REPOS_PATH)
 
     nbr_repos_found = 0
+    total_nbr_repos = 0
     for repo in g.search_repositories(QUERY_STR):
+        total_nbr_repos += 1
         try:
+            output_dir = os.path.join(REPOS_PATH, get_repo_name_from_url(repo.clone_url))
+            if os.path.isdir(output_dir):
+                print(f"{output_dir} already exists, skipping cloning.")
+                continue
+
             repo_languages = repo.get_languages()
             if len(repo_languages) != 1 or repo_languages["Java"] < MIN_LINES_NBR:
-                pass
-
-            clone_repo(repo.clone_url, REPOS_PATH)
+                continue
+            if repo.get_contributors().totalCount < MIN_NBR_CONTRIBUTORS:
+                continue
+            if repo.get_commits().totalCount < MIN_NBR_COMMITS:
+                continue
+            clone_repo(repo.clone_url, output_dir)
             nbr_repos_found += 1
         except KeyboardInterrupt:
             break
 
-    print(f"{nbr_repos_found} found.")
+    print(f"{nbr_repos_found} found, out of {total_nbr_repos} repos.")
 
 
 if __name__ == "__main__":
